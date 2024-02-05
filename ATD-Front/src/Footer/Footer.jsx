445206835af574fc531ef36d5c119ec2d81bc231
Arthur <arthur.morelon@gmail.com>
2024-02-05 19:28:39 +0100

    add HomePage

---
+++ ATD-Front/src/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import { useEffect} from "react";
+import { initFlowbite} from "flowbite";
+
+function Footer() {
+
+    const name = "Au Temps Donné"
+    const signal = "Signaler un problème"
+    const mention = "Mentions légales  - Condition d’utilisation"
+
+
+
+    useEffect(() => {
+        initFlowbite();
+    }, []);
+
+    return (
+        <footer className="bg-[#F1F1F1] mt-32 shadow dark:bg-gray-800">
+            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#"
+                                                                                          className="hover:underline">{name}.</a><br/>  {mention}.
+                </span>
+                <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                    <li>
+                        <a href="#" className="hover:underline me-4 md:me-6">{signal}</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
--- ATD-Front/src/Header/Header.jsx
+++ ATD-Front/src/Header/Header.jsx
@@ -1,7 +1,9 @@
 import { useEffect} from "react";
 import { initFlowbite} from "flowbite";
-import logo from '../files/logo.png'
-import burgerBar from '../files/icones/burger-bar.png'
+import logo from '../../files/logo.png'
+import burgerBar from '../../files/icones/menu-burger.svg'
+import magnifyingGlass from '../../files/icones/chercher.svg'
+import './header.css'
 
 function Header() {
 
@@ -23,7 +25,7 @@ function Header() {
 
     return (
 
-        <nav className="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+        <nav className="bg-white  border-gray-200 dark:bg-gray-900 dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src={logo} className="h-16" alt="Flowbite Logo"/>
@@ -33,19 +35,23 @@ function Header() {
                         aria-controls="navbar-dropdown" aria-expanded="false">
                 </button>
                 <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                    <ul className="flex items-center flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                    <ul className="flex align-middle items-center flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="#"
-                               className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
-                               aria-current="page">{donation}</a>
+                               className="btn-first">{donation}</a>
                         </li>
                         <li>
                             <a href="#"
-                               className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{registration}</a>
+                               className="btn-sec">{registration}</a>
                         </li>
                         <li>
                             <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar">
-                                <img src={burgerBar} className="h-16" alt=""/>
+                                <img src={magnifyingGlass} className="h-10" alt=""/>
+                            </button>
+                        </li>
+                        <li>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar">
+                                <img src={burgerBar} className="h-10" alt=""/>
                             </button>
                             <div id="dropdownNavbar"
                                  className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
@@ -69,6 +75,3 @@ function Header() {
 }
 
 export default Header
-
-export class Header {
-}
