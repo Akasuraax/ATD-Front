34775ae79dd4ef0db2ae8156e1c9205ba57f695d
Arthur <arthur.morelon@gmail.com>
2024-02-06 16:17:12 +0100

    Add router

--- ATD-Front/src/App.jsx
+++ ATD-Front/src/App.jsx
@@ -3,8 +3,21 @@ import { initFlowbite} from "flowbite";
 import Header from './components/Header/Header.jsx'
 import Home from './page/Home/Home.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 function App() {
+
+   const router = createBrowserRouter([
+        {
+            path :'/',
+            element: <Home></Home>
+        },
+        {
+            path :'Login',
+            element: <div>Login</div>
+        },
+
+    ])
 
     useEffect(() => {
         initFlowbite();
@@ -14,7 +27,7 @@ function App() {
       <html>
           <body>
               <Header></Header>
-              <Home></Home>
+              <RouterProvider router={router} />
               <Footer></Footer>
           </body>
       </html>
