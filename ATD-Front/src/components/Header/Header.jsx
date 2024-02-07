b67366b76edfae0c24634a20bd094ea2ae01c20f
Arthur <arthur.morelon@gmail.com>
2024-02-07 21:33:18 +0100

    WIP header

--- ATD-Front/src/components/Header/Header.jsx
+++ ATD-Front/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect} from "react";
+import {useEffect, useState} from "react";
 import { initFlowbite} from "flowbite";
 import logo from '../../../files/logo.png'
 import burgerBar from '../../../files/icones/menu-burger.svg'
@@ -9,14 +9,24 @@ function Header() {
 
 
     const donation  = "Je fais un don";
-    const registration = "Je m'inscris";
+    const registration = "Connexion";
 
-    const menu = [
-        "Nos activités",
-        "Évènements à venir",
-        "Nos locaux",
-        "À propos"
-    ]
+    const menu = [{
+        name :  "Nos activités",
+        link:   ""
+    },
+    {
+        name :  "Évènements à venir",
+        link:   ""
+    },
+    {
+        name :  "Nos locaux",
+        link:   ""
+    },
+    {
+        name :  "À propos",
+        link:   ""
+    }]
 
 
     useEffect(() => {
@@ -25,51 +35,74 @@ function Header() {
 
     return (
 
-        <nav className="bg-white  border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+
+        <nav className="bg-white border-gray-200 dark:bg-gray-900">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src={logo} className="h-16" alt="Flowbite Logo"/>
+                <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <span
+                        className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Au Temps Donnée</span>
                 </a>
-                <button data-collapse-toggle="navbar-dropdown" type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-dropdown" aria-expanded="false">
-                </button>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-                    <ul className="flex align-middle items-center flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-12 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    <button type="button"
+                            className="mr-8 text-white bg-[#F85866] hover:bg-[#E84856] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        {registration}
+                    </button>
+                    <button data-collapse-toggle="navbar-cta" type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="navbar-cta" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M1 1h15M1 7h15M1 13h15"/>
+                        </svg>
+                    </button>
+                </div>
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                     id="navbar-cta">
+                    <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        {menu.map((menu) => (
+                            <li>
+                                <a href={menu.link}
+                                   className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#E84856] md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{menu.name}</a>
+                            </li>
+                        ))}
                         <li>
-                            <a href="#"
-                               className="btn-first">{donation}</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                               className="btn-sec">{registration}</a>
-                        </li>
-                        <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar">
-                                <img src={magnifyingGlass} className="h-10" alt=""/>
-                            </button>
-                        </li>
-                        <li>
-                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar">
-                                <img src={burgerBar} className="h-10" alt=""/>
-                            </button>
+                            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                                    className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-[#E84856] md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Dropdown <svg
+                                className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round"
+                                      strokeWidth="2" d="m1 1 4 4 4-4"/>
+                            </svg></button>
                             <div id="dropdownNavbar"
                                  className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-400"
                                     aria-labelledby="dropdownLargeButton">
-                                    {menu.map(menu => <li>
+                                    <li>
                                         <a href="#"
-                                           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{menu}</a>
-                                    </li>)}
-
+                                           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                                    </li>
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                                    </li>
                                 </ul>
+                                <div className="py-1">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
+                                        out</a>
+                                </div>
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
 
     )
 }
