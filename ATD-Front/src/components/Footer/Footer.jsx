17803d22161c4e43b3703ecafe015c7a64da6eba
Arthur <arthur.morelon@gmail.com>
2024-02-09 12:53:22 +0100

    fix Footer navLink

--- ATD-Front/src/components/Footer/Footer.jsx
+++ ATD-Front/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import { useEffect} from "react";
 import { initFlowbite} from "flowbite";
-import {NavLink} from "react-router-dom";
+import {Navigate, NavLink} from "react-router-dom";
 
 function Footer() {
 
@@ -17,8 +17,8 @@ function Footer() {
     return (
         <footer className="bg-[#F1F1F1] shadow dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="#"
-                                                                                          className="hover:underline">{name}.</a><br/>  {mention}.
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <NavLink to="/"
+                                                                                          className="hover:underline">{name}.</NavLink><br/>  {mention}.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
