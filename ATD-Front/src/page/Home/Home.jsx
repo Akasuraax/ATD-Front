a94c8f5f172dce984b36132ce74a59134e59a9ba
Akas <phelina94@gmail.com>
2024-02-11 17:03:16 +0100

    fix language

---
+++ ATD-Front/src/page/Home/Home.jsx
@@ -0,0 +1,141 @@
+import './home.css'
+import imageHome from '../../../files/image/chaine-696x419.png'
+import abc from '../../../files/image/abc.png'
+import admin from '../../../files/image/admin.png'
+import course from '../../../files/image/course.png'
+import distribution from '../../../files/image/distribution.png'
+import donation from '../../../files/image/donation.png'
+import emplacement from '../../../files/image/emplacement.png'
+import oldCare from '../../../files/image/old-care.png'
+import {Link} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+
+function Home() {
+
+    const { t } = useTranslation();
+
+    const homeTextImage = t("home.homeTextImage");
+    const registration = t("home.registration");
+    const homeText = t("home.homeText");
+    const homeTitle = t("home.homeTitle");
+    const infos = t("home.infos");
+
+
+    return (
+        <div>
+            <div className="image">
+                <text className="homeText">{homeTextImage}</text>
+                <Link to="/register"
+                   className="btn-first homeButton">{registration}</Link>
+            </div>
+
+            <div className="homeBody max-w-screen-xl flex flex-nowrap justify-between mx-auto">
+                <img className="imageHome" src={imageHome} alt=""></img>
+                <div className="pl-16">
+                    <title1 className="homeTitle">{homeTitle}</title1>
+                    <p className="pt-16">{homeText}</p>
+                    <a href="#"
+                       className="btn-first mt-8">{infos}</a>
+                </div>
+            </div>
+
+            <Activity></Activity>
+        </div>
+    )
+}
+
+function Activity() {
+
+
+    const titleActivity = "Nos activités";
+    const foodDistribution = "Distribution alimentaire";
+    const administrativeDepartment = "Service administratif";
+    const shuttles = "Navettes pour rendez-vous éloignés";
+    const courses  = "Cours d’alphabétisation pour adultes";
+    const tutoring  = "Soutien scolaire pour enfant";
+    const event = "Organisation d’évènements de récoltes de fonds";
+    const seniors = "Visites et activités pour personnes âgées.";
+
+    return (
+        <div className="bg-[#F1F1F1] py-12 sm:py-12 mt-32">
+            <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                <h2 className="text-center homeTitle">{titleActivity}</h2>
+                <div
+                    className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+                            src={abc}
+                            alt="Transistor"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{tutoring}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+                            src={admin}
+                            alt="Reform"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{administrativeDepartment}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 max-h-24 w-full object-contain lg:col-span-1"
+                            src={course}
+                            alt="Tuple"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{courses}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 max-h-24 w-full object-contain sm:col-start-2 lg:col-span-1"
+                            src={distribution}
+                            alt="SavvyCal"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{foodDistribution}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src={donation}
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{event}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src={emplacement}
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4">{shuttles}</p>
+                    </div>
+                    <div className="text-center	">
+                        <img
+                            className="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1"
+                            src={oldCare}
+                            alt="Statamic"
+                            width={158}
+                            height={48}
+                        />
+                        <p className="m-4 font">{seniors}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ ATD-Front/src/page/Register/Register.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './register.css';
+import {redirect, useNavigate} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+
+
+function Register() {
+    const { t } = useTranslation();
+
+    const partnerText = t("register.partnerText");
+    const beneficiaryText = t("register.beneficiaryText");
+    const volunteerText = t("register.volunteerText");
+
+    const btnPartner = t("register.btnPartner");
+    const btnVolunteer = t("register.btnVolunteer");
+    const btnBeneficiary = t("register.btnBeneficiary");
+
+    const navigate = useNavigate();
+    const toLink = (link) => {
+        navigate("/register/" + link);
+    }
+
+    return (
+        <div className="register-images">
+            <div className="partner">
+                <p className="text-white">{partnerText}</p>
+                <button type="button" onClick={() => toLink("partenaire")}
+                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">{btnPartner}</button>
+            </div>
+            <div className="beneficiary">
+                <p className="text-white">{beneficiaryText}</p>
+                <button type="button" onClick={()=>toLink("beneficiaire")}
+                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">{btnBeneficiary}</button>
+            </div>
+            <div className="volunteer">
+                <p className="text-white">{volunteerText}</p>
+                <button type="button" onClick={() => toLink("benevole")}
+                        className="text-white font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 ">{btnVolunteer}</button>
+            </div>
+        </div>
+    );
+}
+
+export default Register;
