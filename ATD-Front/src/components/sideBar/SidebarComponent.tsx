162a020b1d469c69c07b15aea65e0281cdfab808
Arthur <arthur.morelon@gmail.com>
2024-03-10 14:47:07 +0100

    Fix after merge

--- ATD-Front/src/components/sideBar/SidebarComponent.tsx
+++ ATD-Front/src/components/sideBar/SidebarComponent.tsx
@@ -113,7 +113,7 @@ export default function SidebarComponent() {
                                         </NavLink>
                                     </li>
                                     <li>
-                                        <NavLink to={"/back/warehouse"}
+                                        <NavLink to={"/back/warehouses"}
                                                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                             <i className="fi fi-sr-warehouse-alt"></i>
                                             <span className="ms-3">{t("sidebar.warehouses")}</span>
