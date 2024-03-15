1342de69e62c3a82ecbb0d5f9fdc3610fcb4c65d
Arthur <arthur.morelon@gmail.com>
2024-03-15 12:57:21 +0100

    fix planning

--- ATD-Front/src/page/Planning/Planning.tsx
+++ ATD-Front/src/page/Planning/Planning.tsx
@@ -1,55 +1,40 @@
 import {useTranslation} from "react-i18next";
 import {useParams} from "react-router-dom";
 import Calendar from "../../components/Calendar/Calendar";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import {getUser} from "../../apiService/UserService";
 import {IActivity} from "../../interfaces/activity";
 import {getActivitiesBetween} from "../../apiService/ActivityService";
 import {useToast} from "../../components/Toast/ToastContex";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import {formatDate} from "@fullcalendar/core";
+import {Spinner} from "flowbite-react";
 
 export default function Planning() {
 
     const {t} = useTranslation();
     const {userId} = useParams();
     const [activities, setActivities] = useState<IActivity[] | null>([]);
-    const [standBy, setStandBy] = useState(true);
+    const [standBy, setStandBy] = useState(false);
     const {pushToast} = useToast();
 
-
-    useEffect(() => {
-        request();
-    }, []);
-
-
-    async function request() {
-        const date = {
-            startDate: "2024-03-10",
-            endDate: "2024-03-20"
-        }
-        setStandBy(true);
-        try {
-            const activityResponse = await getActivitiesBetween(date, pushToast);
-            console.log(activityResponse)
-            setActivities(activityResponse.data)
-        } catch (error) {
-            setStandBy(true);
-        }
-    }
 
     async function handleDatesSet(e) {
         const date = {
             startDate: e.startStr,
             endDate: e.endStr
         }
-        setStandBy(true);
         try {
             const activityResponse = await getActivitiesBetween(date, pushToast);
-            setActivities(activityResponse.data)
-            console.log(activities)
-            setStandBy(false)
+            setActivities(activityResponse)
         } catch (error) {
-            setStandBy(true);
         }
+    }
+
+    function handleEventClick(clickInfo) {
     }
 
     return (
@@ -57,11 +42,35 @@ export default function Planning() {
             <div
                 style={{width: '80vw'}}
                 className="flex flex-wrap min-w-full items-center mx-auto">
-                <Calendar
-                handleDatesSet={handleDatesSet}
-                />
+                <div className='w-full'>
+                    <FullCalendar
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek'
+                        }}
+                        initialView='timeGridWeek'
+                        height={"70vh"}
+                        selectable={false}
+                        datesSet={handleDatesSet}
+                        weekends={true}
+                        events={activities}
+                        eventContent={renderEventContent} // custom render function
+                        eventClick={handleEventClick}
+                    />
+                </div>
             </div>
         </main>
     )
 
+    function renderEventContent(eventInfo) {
+        return (
+            <>
+                <b>{eventInfo.timeText}</b>
+                <h1 className="md:text-lg">{eventInfo.event.title}</h1>
+                <p>{eventInfo.event.extendedProps.description}</p>
+            </>
+        )
+    }
 }
