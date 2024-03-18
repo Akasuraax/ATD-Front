9223a52c833b9479b5f1919a047e48d43e331314
Arthur <arthur.morelon@gmail.com>
2024-03-18 22:20:45 +0100

    add recipes in sidebar

--- ATD-Front/src/components/sideBar/SidebarComponent.tsx
+++ ATD-Front/src/components/sideBar/SidebarComponent.tsx
@@ -120,6 +120,13 @@ export default function SidebarComponent() {
                                             <span className="ms-3">{t("sidebar.warehouses")}</span>
                                         </NavLink>
                                     </li>
+                                    <li>
+                                        <NavLink to={"/back/recipes"}
+                                                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                            <i class="fi fi-rr-ballot"></i>
+                                            <span className="ms-3">{t("sidebar.recipes")}</span>
+                                        </NavLink>
+                                    </li>
                                 </ul>
                             ) : null}
                         </div>
