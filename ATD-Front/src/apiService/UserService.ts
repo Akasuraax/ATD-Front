559f2661a1ce2d5196f076644a72c68e5c7ee73c
Akas <phelina94@gmail.com>
2024-03-22 18:34:30 +0100

    can download files

--- ATD-Front/src/apiService/UserService.ts
+++ ATD-Front/src/apiService/UserService.ts
@@ -1,4 +1,7 @@
-import {getRequest, deleteRequest, patchRequest, postRequest, logIn} from './apiService.js';
+import {deleteRequest, getRequest, logIn, patchRequest, postRequest} from './apiService.js';
+import axios from "axios";
+
+const API_BASE_URL = 'http://127.0.0.1:8000/api';
 
 
 export const postUser = async (params, pushToast, url) => {
@@ -27,3 +30,25 @@ export const patchUser = async (params, pushToast,userId) => {
 export const patchUserAdmin = async (params, pushToast,userId) => {
     return patchRequest(`user/admin/${userId}`,params, pushToast);
 };
+export const getUserFiles = async (params, pushToast) => {
+    return getRequest('user/' + params + '/file','', pushToast);
+};
+
+export const downloadFile = async (params, pushToast) => {
+    try {
+        return await axios.get(`${API_BASE_URL}/file/${params}`, {
+            headers: {
+                "Content-Type": ["application/pdf", "image/png", "image/jpeg", "image/jpg"]
+            },
+            responseType: 'blob'
+        });
+    } catch (error) {
+        console.error('Error downloading file:', error);
+        pushToast({
+            content: "An error occurred while fetching the file",
+            type: "failure"
+        });
+        return null;
+    }
+};
+
--- ATD-Front/src/components/profile/Files.tsx
+++ ATD-Front/src/components/profile/Files.tsx
@@ -1,8 +1,46 @@
 import {IUser} from "../../interfaces/user";
 import {useTranslation} from "react-i18next";
 import {PaperClipIcon} from "@heroicons/react/20/solid";
+import {getUserFiles, downloadFile} from "../../apiService/UserService";
+import {useToast} from "../Toast/ToastContex";
+import {useEffect, useState} from "react";
+import axios from 'axios'
 
 export default function Files({user}: { user: IUser }) {
+    const { pushToast } = useToast();
+    const [files, setFiles] = useState([]);
+
+    useEffect(() => {
+        sendRequest();
+    }, []);
+
+    async function sendRequest(){
+        try{
+            const response = await getUserFiles(user.id, pushToast);
+            setFiles(response.data);
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    async function downloadUserFile(id, filename) {
+        try {
+            const response = await downloadFile(id);
+            const fileUrl = window.URL.createObjectURL(new Blob([response.data]));
+
+            const link = document.createElement('a');
+            link.href = fileUrl;
+            link.setAttribute('download', filename);
+
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+            URL.revokeObjectURL(fileUrl);
+        } catch (error) {
+            console.error('Error downloading file:', error);
+        }
+    }
 
     const {t} = useTranslation();
 
@@ -10,61 +48,28 @@ export default function Files({user}: { user: IUser }) {
         <div
             className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg p-8  md:p-12 mb-8">
             <h1 className="text-gray-900 dark:text-white text-3xl md:text-3xl font-extrabold mb-4">{t("user.file")}</h1>
-            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-0.5">Revenue
-                fiscal</p>
-            <div className="divide-y divide-gray-100 rounded-md border mb-4 border-gray-200">
-                <div className="flex items-center justify-between p-4 text-sm leading-6">
-                    <div className="flex w-0 flex-1 items-center">
-                        <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400 "
-                                       aria-hidden="true"/>
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2 pl-2">
-                                                <span
-                                                    className="truncate font-medium">Revenue fiscal.pdf</span>
-                            <span className="flex-shrink-0 text-gray-400">2.4mb</span>
+            {files.map(function(data){
+                return (
+                    <>
+                        <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-0.5">{data["name"]}</p>
+                        <div className="divide-y divide-gray-100 rounded-md border mb-4 border-gray-200">
+                            <div className="flex items-center justify-between p-4 text-sm leading-6">
+                                <div className="flex w-0 flex-1 items-center">
+                                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400 "
+                                                   aria-hidden="true"/>
+                                    <div className="ml-4 flex min-w-0 flex-1 gap-2 pl-2">
+                                    <span
+                                        className="truncate font-medium">{data["link"].replace(/.*\//, '')}</span>
+                                    </div>
+                                </div>
+                                <div className="ml-4 pl-2 flex-shrink-0">
+                                    <button onClick={() => downloadUserFile(data["id"], data["link"].replace(/.*\//, ''))}>Télécharger</button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="ml-4 pl-2 flex-shrink-0">
-                        <button>Télécharger</button>
-                    </div>
-                </div>
-            </div>
-
-            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-0.5">Permis</p>
-            <div className="divide-y divide-gray-100 rounded-md border mb-4 border-gray-200">
-                <div className="flex items-center justify-between p-4 text-sm leading-6">
-                    <div className="flex w-0 flex-1 items-center">
-                        <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400 "
-                                       aria-hidden="true"/>
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2 pl-2">
-                                                <span
-                                                    className="truncate font-medium">Permis.pdf</span>
-                            <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                        </div>
-                    </div>
-                    <div className="ml-4 pl-2 flex-shrink-0">
-                        <button>Télécharger</button>
-                    </div>
-                </div>
-            </div>
-
-            <p className="text-lg font-normal text-gray-500 dark:text-gray-400 mb-0.5">Cassier
-                judiciaire</p>
-            <div className="divide-y divide-gray-100 rounded-md border mb-4 border-gray-200">
-                <div className="flex items-center justify-between p-4 text-sm leading-6">
-                    <div className="flex w-0 flex-1 items-center">
-                        <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400 "
-                                       aria-hidden="true"/>
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2 pl-2">
-                                                <span
-                                                    className="truncate font-medium">Cassier judiciaire.pdf</span>
-                            <span className="flex-shrink-0 text-gray-400">2.4mb</span>
-                        </div>
-                    </div>
-                    <div className="ml-4 pl-2 flex-shrink-0">
-                        <button>Télécharger</button>
-                    </div>
-                </div>
-            </div>
+                    </>
+                )
+            })}
         </div>
     )
 }
