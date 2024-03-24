fd02c77d7d58df4f1855d688e5936597cf9f315d
Akas <phelina94@gmail.com>
2024-03-24 15:02:39 +0100

    fix some console errors

--- ATD-Front/src/page/Home/Home.jsx
+++ ATD-Front/src/page/Home/Home.jsx
@@ -1,4 +1,4 @@
-import './home.css'
+import  './home.css'
 import imageHome from '../../../files/image/chaine-696x419.png'
 import {Link} from "react-router-dom";
 import {useTranslation} from "react-i18next";
@@ -19,7 +19,7 @@ function Home() {
         <main>
             <div className="home-page">
                 <div className="image">
-                    <text className="homeText">{homeTextImage}</text>
+                    <p className="homeText">{homeTextImage}</p>
                     <Link to="/register"
                        className="btn-first hover:bg-[#E84856] homeButton">{registration}</Link>
                 </div>
@@ -27,7 +27,7 @@ function Home() {
                 <div className="homeBody max-w-screen-xl flex flex-nowrap justify-between mx-auto">
                     <img className="imageHome" src={imageHome} alt=""></img>
                     <div className="pl-16">
-                        <title1 className="homeTitle">{homeTitle}</title1>
+                        <h1 className="homeTitle">{homeTitle}</h1>
                         <p className="pt-16">{homeText}</p>
                         <a href="#"
                            className="btn-first hover:bg-[#E84856] mt-8">{infos}</a>
