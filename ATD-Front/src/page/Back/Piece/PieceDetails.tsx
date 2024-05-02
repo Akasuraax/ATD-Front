1ae7bf47de48e1cd0a13912108e11383af8b93e3
Akas <phelina94@gmail.com>
2024-05-02 22:15:16 +0200

    can't load qr when archived

--- ATD-Front/src/page/Back/Piece/PieceDetails.tsx
+++ ATD-Front/src/page/Back/Piece/PieceDetails.tsx
@@ -339,6 +339,7 @@ export default function PieceDetails(){
                         </div>
                         <div className="m-4 border p-8 rounded-xl shadow-md">
                             <button
+                                disabled={piece.archive}
                                 className={`block w-full text-white ${piece.archive ? 'bg-purple-300 cursor-not-allowed' : 'bg-purple-700 cursor-pointer hover:bg-purple-800'}  focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-6 me-2 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800`}
                                 onClick={() => {
                                    showQr()
@@ -378,9 +379,15 @@ export default function PieceDetails(){
                         </div>
 
                         {qrImageUrl && (
-                            <div className="m-4 border p-8 rounded-xl shadow-md">
+                            <div>
+                                <div className="m-4 border p-8 rounded-xl shadow-md">
                                     <div dangerouslySetInnerHTML={{__html: qrImageUrl}}/>
+                                </div>
+                                <div className="m-4 border p-8 rounded-xl shadow-md">
+                                    <p>Télécharger</p>
+                                </div>
                             </div>
+
                         )}
                     </>
                 ) : (
