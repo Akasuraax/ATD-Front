3aaf62ee7ed9087aa7279059ed6a15a21d2470bf
root <root@vmi1844787.contaboserver.net>
2024-05-04 17:41:34 +0200

    add dockerfile dev

--- ATD-Front/src/apiService/apiService.js
+++ ATD-Front/src/apiService/apiService.js
@@ -1,6 +1,7 @@
 // apiService.js
 import axios from 'axios';
 import Cookies from "js-cookie";
+export const BASE_URL = 'https://eautantdone.com:8000'
 export const API_BASE_URL = 'https://eautantdone.com:8000/api';
 
 const getHeaders = () => {
