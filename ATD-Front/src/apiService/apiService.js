e6e2e7a1c8db8cf05e4412459defd78733a1b495
Akas <phelina94@gmail.com>
2024-05-05 23:04:49 +0200

    fix

--- ATD-Front/src/apiService/apiService.js
+++ ATD-Front/src/apiService/apiService.js
@@ -1,8 +1,8 @@
 // apiService.js
 import axios from 'axios';
 import Cookies from "js-cookie";
-export const BASE_URL = 'https://eautantdone.com:8000'
-export const API_BASE_URL = 'https://eautantdone.com:8000/api';
+export const BASE_URL = 'http://127.0.0.1:8000'
+export const API_BASE_URL = BASE_URL + '/api';
 
 const getHeaders = () => {
     const headers = {
