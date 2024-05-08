68283c5334995a989c7c8b8eadcc6bd9322ad492
Arthur <arthur.morelon@gmail.com>
2024-05-08 22:45:30 +0200

    add prev activity to profile

--- ATD-Front/src/apiService/ActivityService.ts
+++ ATD-Front/src/apiService/ActivityService.ts
@@ -34,6 +34,10 @@ export const getActivitiesUser = async (params, pushToast) => {
     return getRequest(`activity/withParticipate?userId=${params}`,null, pushToast);
 };
 
+export const getBeforeActivitiesUser = async (params, pushToast) => {
+    return getRequest(`activity/user/${params}`,null, pushToast);
+};
+
 export const isUserRegisteredToActivity = async (params,activityId, pushToast) => {
     return getRequest(`activity/participate/${activityId}`,params, pushToast);
 };
